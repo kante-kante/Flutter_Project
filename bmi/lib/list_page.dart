336cf4b825011cdeeb3162f97775396a674c359d
kang <micro.kante@gmail.com>
2021-11-17 22:36:31 +0900

    11주차 3차시 bmi 기록 리스트 처리 intl datetime 라이브러리 적용 현재 로그인된 사용자의 bmi만 보여지도록 처리.

--- bmi/lib/list_page.dart
+++ bmi/lib/list_page.dart
@@ -1,18 +1,72 @@
+import 'package:bmi/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({Key? key}) : super(key: key);
 
 
   @override
-  _ListPageState createState() => _ListPageState();
+  State<ListPage> createState() => _ListPageState();
 }
 
 class _ListPageState extends State<ListPage> {
+  final User? user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center()
+      appBar: AppBar(
+        title: const Text('BMI 기록'),
+      ),
+        // stream builder를 이용하여 비동기 처리 - 일부 데이터를 여러번 가져 올 때 사용
+        body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('bmi').where('uid', isEqualTo: user!.uid!).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+
+            if(!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final docs = snapshot.data!.docs;
+            logger.i(docs);
+
+            return ListView(
+              children: docs.map((doc) {
+                DateTime datetime = DateTime.fromMicrosecondsSinceEpoch(doc['datetime']);
+
+                return ListTile(
+                    leading: Text(DateFormat('yyyy-MM-dd HH:mm').format(datetime)),
+                    title: Text('${doc['text']}(${doc['bmi']})'),
+                    subtitle: Text('키: ${doc['height']}cm, 몸무게: ${doc['weight']}kg'),
+                );
+              }).toList(),
+            );
+          }
+        ),
     );
   }
+
+  @override
+  void initState() {
+    //해당 클래스가 호출되었을떄
+    super.initState();
+
+  }
+  @override
+  void dispose() {
+    // 해당 클래스가 사라질떄
+
+    super.dispose();
+  }
+
 }
